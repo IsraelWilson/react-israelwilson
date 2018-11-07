853cdf939461f86cb76f8d6fbb5a09a2f1bc40a0
Israel Wilson <israelsjwilson@gmail.com>
2018-11-06 22:11:02 -0600

    Working on fetch

--- src/pages/Hobbies/components/Books.js
+++ src/pages/Hobbies/components/Books.js
@@ -3,12 +3,26 @@ import React from 'react';
 export class Books extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {content: "Fantasy"};
+    this.state = {content: []};
   }
 
   componentDidMount() {
     if(this.props.type == "Fantasy") {
-      fetch("../../../php/getDirectoryContents.php");
+      fetch("../../../php/getDirectoryContents.php", {
+        method: "POST",
+        body: {directory: "Hobbies/images/books/Fantasy"}
+        })
+        .then(res => res.json())
+        .then(
+          result => {
+            alert("Success");
+            this.setState({});
+          },
+          error => {
+            alert(error);
+            this.setState({});
+            }
+            )
     } else {
       fetch("../../../php/getDirectoryContents.php");
     }
